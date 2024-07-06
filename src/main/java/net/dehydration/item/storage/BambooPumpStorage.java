85cb040ef6bc5c0c7ce4f367cf4c969f5110ef20
cech12 <cech12@gmail.com>
2024-07-06 21:36:07 +0200

    fix: stacked item interaction with bamboo pump fix: bucket could not be filled with bamboo pump

--- src/main/java/net/dehydration/item/storage/BambooPumpStorage.java
+++ src/main/java/net/dehydration/item/storage/BambooPumpStorage.java
@@ -1,6 +1,7 @@
 package net.dehydration.item.storage;
 
 import net.dehydration.block.entity.BambooPumpEntity;
+import net.dehydration.init.ItemInit;
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
@@ -18,6 +19,11 @@ public class BambooPumpStorage extends SingleStackStorage {
     }
 
     @Override
+    protected int getCapacity(ItemVariant itemVariant) {
+        return 1;
+    }
+
+    @Override
     protected ItemStack getStack() {
         return pumpEntity.getStack(0);
     }
@@ -29,11 +35,9 @@ public class BambooPumpStorage extends SingleStackStorage {
 
     @Override
     protected boolean canInsert(ItemVariant variant) {
-        if (variant.isBlank()) {
-            return false;
-        }
+        //only items with a fluid storage are allowed (or flask item)
         Storage<FluidVariant> storage = ContainerItemContext.withConstant(variant, 1).find(FluidStorage.ITEM);
-        return storage != null && storage.supportsInsertion();
+        return storage != null || variant.getItem() == ItemInit.LEATHER_FLASK;
     }
 
 }
